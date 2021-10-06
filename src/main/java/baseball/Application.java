da92655c2bb28bc056d16275ebf713027428c836
sungwoo <ssw890418@gmail.com>
2021-10-06 20:30:40 +0900

    -refactor:BaseballGameController를 생성해서 실행하도록 수정
    BaseballGameController의 run 메소드를 정적 메소드에서 멤버 메소드로 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
-        BaseballGameController.run();
+        BaseballGameController controller = new BaseballGameController();
+        controller.run();
     }
 }
