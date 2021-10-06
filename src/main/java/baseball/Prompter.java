98fe08ac420899ddc246f99c0ab84a6890ac1f66
sungwoo <ssw890418@gmail.com>
2021-10-06 22:32:10 +0900

    refactor:콘솔(View)에서 처리해야하는 기능 정리
    사용자 입력 기능 및 시스템이 출력하는 기능을 한 클래스로 모았다

--- src/main/java/baseball/Prompter.java
+++ src/main/java/baseball/Prompter.java
@@ -2,35 +2,37 @@ package baseball;
 
 import nextstep.utils.Console;
 
-public class Promp {
+public class Prompter {
+    private static final String errorMessage = "[ERROR]3자리 숫자를 정확히 입력하세요.";
 
-    public static String inputUserNumber() {
+    public static String queryForUserInputNumber() {
         try {
             System.out.print("숫자를 입력해 주세요 : ");
             String userInputNumber = Console.readLine();
             validateUserInput(userInputNumber);
             return userInputNumber;
-        } catch (NumberFormatException e) {
-            System.out.println("[ERROR]숫자만 입력하세요.");
-            return inputUserNumber();
-        } catch (UserInputException e) {
-            System.out.println(e.getMessage());
-            return inputUserNumber();
+        }  catch (UserInputException exception) {
+            System.out.println(exception.getMessage());
+            return queryForUserInputNumber();
         }
     }
 
-    private static void validateUserInput(String userInputNumber) throws UserInputException {
+    private static void validateUserInput(String userInputNumber) throws UserInputException{
         validateNumberForamt(userInputNumber);
         validateNumberLength(userInputNumber);
     }
 
     private static void validateNumberLength(String userInputNumber) throws UserInputException {
         if(userInputNumber.length() != 3)
-            throw new UserInputException("[ERROR]3자리 숫자를 입력하세요.");
+            throw new UserInputException(errorMessage);
     }
 
-    private static void validateNumberForamt(String userInputNumber) {
-        Integer.parseInt(userInputNumber);
+    private static void validateNumberForamt(String userInputNumber) throws UserInputException{
+        try {
+            Integer.parseInt(userInputNumber);
+        } catch (NumberFormatException e) {
+            throw new UserInputException(errorMessage);
+        }
     }
 
     public static String queryForContinuousGame() {
@@ -38,4 +40,35 @@ public class Promp {
         return Console.readLine();
     }
 
+    public static void printResult(Referee referee) {
+        printStrike(referee);
+        printBall(referee);
+        printNothing(referee);
+        System.out.println();
+        if(referee.isThreeStrike()){
+            printThreeStrike();
+        }
+    }
+
+    private static void printThreeStrike() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
+    }
+
+    private static void printStrike(Referee referee) {
+        if(referee.getStrike() > 0) {
+            System.out.print(String.format("%d%s ", referee.getStrike(), "스트라이크"));
+        }
+    }
+
+    private static void printBall(Referee referee) {
+        if(referee.getBall() > 0) {
+            System.out.print(String.format("%d%s  ", referee.getBall(), "볼"));
+        }
+    }
+
+    private static void printNothing(Referee referee) {
+        if(referee.getNothing() == 3) {
+            System.out.print("낫싱");
+        }
+    }
 }
